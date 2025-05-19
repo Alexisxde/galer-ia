f8f533bf872526ff53e104884d8a6b53b7b5a4ee
Alexisxde <olivarezalexis749@gmail.com>
2025-05-19 18:56:51 -0300

    Actualizar la barra lateral para mostrar el nombre "GalerIA", agregar un nuevo ícono de foto y eliminar el ícono de carga. Modificar la API de galería para eliminar el límite de imágenes y ajustar la lógica de recuperación en la página de comunidad. Añadir un botón de generación en la página de inicio.

--- src/pages/api/gallery/index.ts
+++ src/pages/api/gallery/index.ts
@@ -2,12 +2,8 @@ import { db } from "@/db"
 import { ImagesTable } from "@/db/schema"
 import type { APIRoute } from "astro"
 
-export const GET: APIRoute = async ({ url }) => {
-	const limit = url.searchParams.get("limit")
-	const images = await db
-		.select()
-		.from(ImagesTable)
-		.limit(Number(limit) ?? 20)
+export const GET: APIRoute = async () => {
+	const images = await db.select().from(ImagesTable)
 
 	return new Response(JSON.stringify(images), {
 		headers: { "Content-Type": "application/json" }
